d0d2d79d54d6110ff431863e0e228f3402a334ab
Shaik081 <shaikmas128@gmail.com>
2025-10-05 20:16:23 +0530

    db changed

--- DB/mongoDB.js
+++ DB/mongoDB.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose')
 
 const mongoDB = async () => {
         try{
-            const con = await mongoose.connect(process.env.MONGO_URL)
-            console.log('mongodb is connected');
+            const con = await mongoose.connect(process.env.MONGO_URL,{
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
+            .then(()=>console.log('mongodb is connected'));
             
         }catch(err){
             console.log(`error ${err}`)
